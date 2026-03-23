0f906a7d901a99879918578b5a1da47d1f226844
Nils Ove Tendenes <nils.ove@tendenes.com>
2026-03-23 14:59:18 +0100

    chore: update spring boot & refactor kafka config

--- src/main/kotlin/no/digdir/fdk/mqa/dcatvalidator/configuration/KafkaConsumerConfig.kt
+++ src/main/kotlin/no/digdir/fdk/mqa/dcatvalidator/configuration/KafkaConsumerConfig.kt
@@ -1,20 +1,47 @@
 package no.digdir.fdk.mqa.dcatvalidator.configuration
 
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
 import no.fdk.mqa.DatasetEvent
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.listener.ContainerProperties
 
 @EnableKafka
 @Configuration
-open class KafkaConsumerConfig {
+open class KafkaConsumerConfig(
+    @param:Value("\${spring.kafka.bootstrap-servers}") private val bootstrapServers: String,
+    @param:Value("\${spring.kafka.properties.schema.registry.url}") private val schemaRegistryUrl: String,
+) {
+
+    @Bean
+    open fun consumerFactory(): ConsumerFactory<String, DatasetEvent> {
+        val props: MutableMap<String, Any> = HashMap()
+        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
+        props[ConsumerConfig.GROUP_ID_CONFIG] = "fdk-mqa-dcat-validator"
+        props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = KafkaAvroDeserializer::class.java
+        props[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
+        props[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = false
+        props[ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG] = 2097152
+        props["schema.registry.url"] = schemaRegistryUrl
+        props["specific.avro.reader"] = true
+        props["auto.register.schemas"] = false
+        props["use.latest.version"] = true
+        props["value.subject.name.strategy"] = "io.confluent.kafka.serializers.subject.RecordNameStrategy"
+        props["key.subject.name.strategy"] = "io.confluent.kafka.serializers.subject.RecordNameStrategy"
+        return DefaultKafkaConsumerFactory(props)
+    }
 
     @Bean
     open fun kafkaListenerContainerFactory(consumerFactory: ConsumerFactory<String, DatasetEvent>): ConcurrentKafkaListenerContainerFactory<String, DatasetEvent> {
-        val factory: ConcurrentKafkaListenerContainerFactory<String, DatasetEvent> = ConcurrentKafkaListenerContainerFactory()
+        val factory = ConcurrentKafkaListenerContainerFactory<String, DatasetEvent>()
         factory.setConsumerFactory(consumerFactory)
         factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL_IMMEDIATE
         return factory
